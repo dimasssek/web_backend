391de2b90a855350686ab0f0c4ffc3b3c58e7e1d
JuliusGer <78736926+JuliusGer@users.noreply.github.com>
2022-03-20 03:56:10 +0300

    Update form_handler.php

--- task-3/form_handler.php
+++ task-3/form_handler.php
@@ -1,19 +1,54 @@
 <?php
-	
-//$name = '';
-$ready_to_send = true;
- foreach( $_POST as $key => $value){
-    if(empty($value)){
-		echo "Input " . $key . "!</br>";
-		$ready_to_send = false;
-	}
-	else if($key=='super') $super = serialize($_POST['super']);
+// Отправляем браузеру правильную кодировку,
+// файл index.php должен быть в кодировке UTF-8 без BOM.
+header('Content-Type: text/html; charset=UTF-8');
+
+// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
+  if (!empty($_GET['save'])) {
+    // Если есть параметр save, то выводим сообщение пользователю.
+    print('Спасибо, результаты сохранены.');
+  }
+  // Включаем содержимое файла form.php.
+  include('index.php');
+  // Завершаем работу скрипта.
+  exit();
+}
+// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
+
+// Проверяем ошибки.
+$errors = FALSE;
+if (empty($_POST['name'])) {
+  print('Заполните имя.<br/>');
+  $errors = TRUE;
+}
+
+if (empty($_POST['email']) or !(strpos($_POST['email'], '@'))) {
+  print('Введите e-mail.<br/>');
+  $errors = TRUE;
+}
+
+if (empty($_POST['year'])) {
+  print('Выберите год рождения.<br/>');
+  $errors = TRUE;
+}
+
+if (empty($_POST['gender'])) {
+  print('Укажите ваш пол.<br/>');
+  $errors = TRUE;
 }
 
 
-/*
+if (empty($_POST['kon'])){
+    print ('Выберите количество конечностей.<br>');
+    $errors = true;
+
+}
+
 if (empty($_POST['super'])){
-    print ('Выберите хотя бы одну сверхспособность.<br>');
+    print ('Выберите одну или несколько сверхспособностей.<br>');
     $errors = true;
 
 }
@@ -21,65 +56,69 @@ else {
   $super = serialize($_POST['super']);
 }
 
-*/
-if(!$ready_to_send){
-	exit();
+if (empty($_POST['bio'])){
+    print ('Расскажите о себе.<br>');
+    $errors = true;
 }
 
-$user = 'u47569'; // Логин от БД
-$pass = '3312824'; // Пароль от БД
-
-
-
-$conn = new PDO('mysql:host=localhost;dbname=u47569', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
-if ($conn === false) {
-  die("Ошибка: " . mysqli_connect_error());
-} 
-echo "Подключение успешно установлено";
-mysqli_close($conn);
-
-// Создаем класс подключения к БД
-/*try {
-  $db = new PDO("mysql:host=localhost;dbname=u47569", $user, $pass);
-} catch (PDOException $e) {
-  print "Error!: " . $e->getMessage();
-  die();
-}*/
-$sql = "INSERT INTO Superpowers (name)
-VALUES ('Бессмертие'), ('Способность проходить через стены'), ('Левитация')";
-
-if ($bd->query($sql) === TRUE) {
-   echo "Успешно создана новая запись";
-} else {
-   echo "Ошибка: " . $sql . "<br>" . $conn->error;
+if (empty($_POST['contr_check'])){
+    print ('Вы не можете отправить форму, не ознакомившись с контрактом.<br>');
+    $errors = true;
 }
+
+if ($errors) {
+  // При наличии ошибок завершаем работу скрипта.
+  exit();
+}
+
+// Сохранение в базу данных.
+
+$user = 'u47569';
+$pass = '3312824';
+$db = new PDO('mysql:host=localhost;dbname=u47569', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 
 // Подготовленный запрос. Не именованные метки.
-try{
-    $stmt = $db->prepare("INSERT INTO application SET name = ?, email = ?, year = ?, gender = ?, con = ?, super = ?, bio = ?, contr_check = ?");
-    $stmt->execute(array(
-        $_POST['name'],
+
+try {
+  $stmt = $db->prepare("INSERT INTO application SET name = ?, email = ?, year = ?, gender = ?, kon = ?, bio = ?, contr_check = ?");
+  $stmt -> execute(array(
+		$_POST['name'],
         $_POST['email'],
         $_POST['year'],
         $_POST['gender'],
-        $_POST['con'],
-        $super,
+        $_POST['kon'],
         $_POST['bio'],
-		$_POST['contr_check'],
-    ));
+		$_POST[true],
+	));
+	
+  $stmt = $db->prepare("INSERT INTO Superpowers SET name = ?");
+  $stmt -> execute(array(
+		$_POST['super'] = implode(', ', $_POST['super']),
+	));
 }
 catch(PDOException $e){
-    // При возникновении ошибки отправления в БД, выводим информацию
-    print ('Ошибка: ' . $e->getMessage());
-    exit();
+  print('Error: ' . $e->getMessage());
+  exit();
 }
-/*if(empty($_POST["name"])){
-	print "Введите имя!</br>";
-	$ready_to_send = false;
-}
-if(empty($_POST["email"])){
-	print "Введите e-mail!</br>";
-	$ready_to_send = false;
-}*/
 
-?>
+//  stmt - это "дескриптор состояния".
+ 
+//  Именованные метки.
+//$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
+//$stmt -> execute(array('label'=>'perfect', 'color'=>'green'));
+ 
+//Еще вариант
+/*$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
+$stmt->bindParam(':firstname', $firstname);
+$stmt->bindParam(':lastname', $lastname);
+$stmt->bindParam(':email', $email);
+$firstname = "John";
+$lastname = "Smith";
+$email = "john@test.com";
+$stmt->execute();
+*/
+
+// Делаем перенаправление.
+// Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
+// Если ошибок при этом не видно, то необходимо настроить параметр display_errors для PHP.
+header('Location: ?save=1');
