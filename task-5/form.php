123ca37722db01cf6e0b6a60911e9e6b9209b919
JuliusGer <78736926+JuliusGer@users.noreply.github.com>
2022-04-22 01:35:51 +0300

    Update form.php

--- task-5/form.php
+++ task-5/form.php
@@ -65,7 +65,7 @@
 			<p><button type="submit" value="send">Отправить</button></p>
 
 		</form>
-		<?php if(!empty($_COOKIE[session_name()]) && !empty($_SESSION['login'])) print( '<div id="footer">Вход с логином ' . $_SESSION["login"]. '<br> <a href=logout.php> Выход</a><br></div>');?>
+		<?php if(!empty($_COOKIE[session_name()]) && !empty($_SESSION['login'])) print( '<div id="footer">Вход с логином ' . $_SESSION["login"]. '<br> <a href=login.php?do=logout> Выход</a><br></div>');?>
 	</div>
 </body>
 </html>
